6ea934a3b6fd5cda1501e192322751325f9e2816
Sathiyakumar.V <sathiya@eco33.com>
2020-06-11 16:47:42 +0530

    refined

--- public/js/deviceslist.js
+++ public/js/deviceslist.js
@@ -286,12 +286,12 @@ function buildnavitree()
                         '" placeholder="add a comment..."><br/><button class="btn btn-success btn-anim  btn-rounded" onclick="verdict_saver(\'' + tempname +
                         '\')"><i class="fas fa-plus"></i><span class="btn-text">Add</span></button></div>';
                     var myvar = '<table id="edit_datable_' + tempname + '" class="table table-hover display mb-30 dataTable no-footer"  width="100%" style="cursor: pointer;" role="grid" aria-describedby="edit_datable_2_info">' +
-                        '<thead><div class="row"><div class="col-sm-2 row-mg-left "><div class="pull-left pull-left-mg"><h6 style="float: left"> Last Ticket : </h6> &nbsp; &nbsp;' +
+                        '<thead><div class="row"><div class="col-sm-2 row-mg-left "><div class="pull-left pull-left-mg"><h6 style="float: left" class="dis-inl"s> Last Ticket : </h6> &nbsp; &nbsp;' +
                         ' <h6 class="label label-info text-bg-round" style="float: right" id="currentticket_' + tempname + '">0</h6></div></div><div class="col-sm-8 pull-right tkt-btn"><div class="pull-right pull-right-mg">' +
                         '&nbsp;&nbsp;' +
                         '<button class="btn btn-success tkt-btn btn-anim btn-rounded " onclick="call_ticket_modal(\'' + tempname + "','" + tempicon + '\',\'123\')"  data-toggle="modal" data-target="#open_ticket_modal">' +
                         '<i class="fas fa-ticket-alt"></i><span class="btn-text">Open Ticket</span></button></div></div></div>' +
-                        '<div class="row dis-inl"><div class="col-sm-12 dis-inl row-mg-left"><h6 class="dis-inl">Available Sites : &nbsp;&nbsp&nbsp;&nbsp</h6><div class="dis-inl" id="currentusers_' + tempname + '"</div></div></tr></tbody></table > <select class="form-control tkt-opt-hide" id="combo_' + tempname + '"></select >';
+                        '<div class="row dis-inl"><div class="col-sm-12 dis-inl row-mg-left"><h6 class="dis-inl">Available Users : &nbsp;&nbsp&nbsp;&nbsp</h6><div class="dis-inl" id="currentusers_' + tempname + '"</div></div></tr></tbody></table > <select class="form-control tkt-opt-hide" id="combo_' + tempname + '"></select >';
                     new_tab2 = new_tab2 + '<li role="presentation" class="' + bb + ' margin-top-tkt"><a data-toggle="tab"' + 'id="' + tempname + '1_tab2' + '"  onclick = "reload_table(\'' + tempname + '\')" role="tab"' + 'href="#' + tempname + '_tab2' + '"' +
                         'aria-expanded="true"><i class="' + tempicon + '"></i> &nbsp;&nbsp' + tempname + ' &nbsp; &nbsp; <div class="pull-right"><span class="label label-primary" id="' + tempname
                         + '_label2' + '">0</span></div></a></li>';
